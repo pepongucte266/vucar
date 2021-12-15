fdb5a987b2fa74d4ff16f12ba02801dbaa99fee8
pepongcute266 <pepongcute266@gmail.com>
2021-12-15 14:17:47 +0700

    chapter_6: modeling user

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  before_save :downcase_email
+  validates :name, presence: true, length: {maximum: Settings.max_name}
+  validates :email, presence: true, length: {maximum: Settings.max_email},
+    format: {with: Settings.email_regex},
+    uniqueness: true
+  validates :password, presence: true, length: {minimum: Settings.min_password}
+  has_secure_password
+
+  private
+  def downcase_email
+    email.downcase!
+  end
+end
