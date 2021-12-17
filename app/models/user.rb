c3ad1f21cd651879add5dd71410e0c0e5fb6ea19
VTSon <pepongcute266@gmail.com>
2021-12-17 10:21:26 +0700

    chapter 8: Basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   validates :password, presence: true, length: {minimum: Settings.min_password}
   has_secure_password
 
+  def self.digest string
+    cost =  if ActiveModel::SecurePassword.min_cost
+              BCrypt::Engine::MIN_COST
+            else
+              BCrypt::Engine.cost
+            end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   private
   def downcase_email
     email.downcase!
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
     get "/home", to: "static_pages#home"
     get "/contact", to: "static_pages#contact"
     get "/signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
     resources :users
     # resources :users, only: %i(new create)
   end
